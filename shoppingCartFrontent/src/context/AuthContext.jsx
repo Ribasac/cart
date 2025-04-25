e868e79165e55f0e1e80b96e2e7bc08136c29cd4
ribasac <ribasksd@gmail.com>
2025-04-25 22:01:34 +0530

    final

--- shoppingCartFrontent/src/context/AuthContext.jsx
+++ shoppingCartFrontent/src/context/AuthContext.jsx
@@ -7,33 +7,41 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(false);
 
     const signup = async (user) => {
-        const signupResponse = await api.post("/auth/signup", user);
+        try {
+            const signupResponse = await api.post("/auth/signup", user);
 
-        if (signupResponse.data.token) {
-            const token = signupResponse.data.token;
-            console.log(token);
-            localStorage.setItem("token", token);
-            localStorage.setItem("user", true);
-            setUser(true);
-            return token;
+            if (signupResponse.data.token) {
+                const token = signupResponse.data.token;
+                console.log(token);
+                localStorage.setItem("token", token);
+                localStorage.setItem("user", true);
+                setUser(true);
+                return token;
+            }
+        } catch (e) {
+            alert("Error occured", e);
         }
 
         return null;
     };
 
     const login = async (email, password) => {
-        const loginResponse = await api.post("/auth/login", {
-            email,
-            password,
-        });
+        try {
+            const loginResponse = await api.post("/auth/login", {
+                email,
+                password,
+            });
 
-        if (loginResponse.data.token) {
-            const token = loginResponse.data.token;
-            console.log(token);
-            localStorage.setItem("token", token);
-            localStorage.setItem("user", true);
-            setUser(true);
-            return token;
+            if (loginResponse.data.token) {
+                const token = loginResponse.data.token;
+                console.log(token);
+                localStorage.setItem("token", token);
+                localStorage.setItem("user", true);
+                setUser(true);
+                return token;
+            }
+        } catch (e) {
+            alert("error occured", e);
         }
 
         return null;
